842402a0b9c6eeff88b3d0f5f29a4df5db1fef59
okuku-blip <okuku@terpmail.umd.edu>
2026-04-08 20:50:53 -0400

    Delete hashgen.py

--- hashgen.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.security import generate_password_hash #always use hashed passwords for security in DB
-
-print(generate_password_hash("Admin1"))
-print(generate_password_hash("Resident1"))
-print(generate_password_hash("Staff1"))
